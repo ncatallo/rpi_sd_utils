9d18e7441ac99ae4c61b1a7d4ec00bfe3be90133
Nicolas Catallo <64967911+ncatallo@users.noreply.github.com>
2021-11-06 18:47:52 +0100

    Added +x option for rpiSdUtils.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 
 # Getting pi shrinker script
 wget  https://raw.githubusercontent.com/Drewsif/PiShrink/master/pishrink.sh
-chmod +x pishrink.sh
 
 # Saving script and installing on the machine
-cp pishrink.sh ./scripts
-sudo mv pishrink.sh /usr/local/bin
+chmod +x pishrink.sh
+sudo cp pishrink.sh /usr/local/bin
 
 # Putting util script to /usr/local/bin
+chmod +x rpiSdUtils.sh
 sudo cp rpiSdUtils.sh /usr/local/bin
