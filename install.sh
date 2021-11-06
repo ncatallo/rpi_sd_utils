1ccd8bd976e0f8413826b68ae0ef49657372664e
Nicolas Catallo <64967911+ncatallo@users.noreply.github.com>
2021-11-06 18:30:10 +0100

    Add util fie to /usr/local/bin

--- install.sh
+++ install.sh
@@ -7,3 +7,6 @@ chmod +x pishrink.sh
 # Saving script and installing on the machine
 cp pishrink.sh ./scripts
 sudo mv pishrink.sh /usr/local/bin
+
+# Putting util script to /usr/local/bin
+sudo cp rpiSdUtils.sh /usr/local/bin
